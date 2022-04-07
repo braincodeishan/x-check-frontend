f0fbb7fa1c3ae0932e758f2722b5a5d7a54923cd
braincodeishan <braincodeishan@gmail.com>
2022-04-07 11:51:41 +0530

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ import ForgotPassword from "./Components/MainComponents/Body/ForgotPassword";
 import Register from "./Components/MainComponents/Body/Register";
 import Wishlist from "./Components/MainComponents/Body/Wishlist";
 import ContactUs from "./Components/MainComponents/Body/ContactUs";
+import Dashboard from './Components/Admin/Dashboard'
+import UpdateMobile from "./Components/Admin/UpdateMobile";
 
 function App() {
   return (
@@ -41,37 +43,39 @@ function App() {
       <LoginProvider>
         <MiscProvider>
           <MobileDataProvider>
-            
-              <Navbar />
-              <div className="body">
-                <Header />
-                <Routes>
-                  <Route exact path="/" element={<Search />} />
-                  <Route exact path="/Login" element={<Login />} />
-                  <Route exact path="/Register" element={<Register />} />
-                  <Route
-                    exact
-                    path="/Forgot-Passwords"
-                    element={<ForgotPassword />}
-                  />
-                  <Route exact path="/Result" element={<Results />} />
-                  <Route
-                    exact
-                    path="/PhoneDetails/:id"
-                    element={<PhoneDetails />}
-                  />
-                  <Route exact path="/Compare" element={<ComparePhones />} />
-                  <Route exact path="/Contact" element={<ContactUs />} />
 
-                  <Route element={<ProtectedRoutes />}>
-                    <Route exact path="/Wishlist" element={<Wishlist />} />
-                  </Route>
-                </Routes>
-              </div>
-              <CompareIcons />
-              <LoveIndia />
-              <Footer />
-            
+            <Navbar />
+            <div className="body">
+              <Header />
+              <Routes>
+                <Route exact path="/" element={<Search />} />
+                <Route exact path="/Login" element={<Login />} />
+                <Route exact path="/Register" element={<Register />} />
+                <Route
+                  exact
+                  path="/Forgot-Passwords"
+                  element={<ForgotPassword />}
+                />
+                <Route exact path="/Result" element={<Results />} />
+                <Route
+                  exact
+                  path="/PhoneDetails/:id"
+                  element={<PhoneDetails />}
+                />
+                <Route exact path="/Compare" element={<ComparePhones />} />
+                <Route exact path="/Contact" element={<ContactUs />} />
+
+                  <Route exact path="/AdminDashboard" element={<Dashboard />} />
+                  <Route exact path="/UpdateMobiles" element={<UpdateMobile />} />
+                <Route element={<ProtectedRoutes />}>
+                  <Route exact path="/Wishlist" element={<Wishlist />} />
+                </Route>
+              </Routes>
+            </div>
+            <CompareIcons />
+            <LoveIndia />
+            <Footer />
+
           </MobileDataProvider>
         </MiscProvider>
       </LoginProvider>

7aee4473cc61d9b13a7c9fe2152da8ae57959de5
braincodeishan <braincodeishan@gmail.com>
2022-04-07 12:22:01 +0530

    --- Auto-Git Commit ---

--- src/Components/Admin/UpdateMobile.js
+++ src/Components/Admin/UpdateMobile.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 const UpdateMobile = () => {
-  return (
-    <div>UpdateMobile</div>
-  )
+    const [data, setData] = useState({});
+    useEffect(() => {
+        getData()
+    }, [data])
+
+    const getData = async () => {
+        const result = await axios.get('http://localhost:3001/updateMobile')
+        if (result) {
+            setData(result.data)
+        }
+    }
+
+
+
+
+    return (
+        <div className="container">
+            <div className="boxLayout">
+                <h2>{data.name}</h2>
+            </div>
+            {!data.twoG && < div className="boxLayout">
+
+            </div>}
+            {!data.threeG && <div className="boxLayout">
+
+            </div>}
+            {!data.fourG && <div className="boxLayout"></div>}
+            {!data.fiveG && <div className="boxLayout"></div>}
+            {data.announced===null && <div className="boxLayout"></div>}
+            <div className="boxLayout"></div>
+            <div className="boxLayout"></div>
+            <div className="boxLayout"></div>
+            <div className="boxLayout"></div>
+            <div className="boxLayout"></div>
+        </div >
+    )
 }
 
 export default UpdateMobile

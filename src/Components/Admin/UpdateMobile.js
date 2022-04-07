faea2b3681201289a62457a6af1e86d035558016
braincodeishan <braincodeishan@gmail.com>
2022-04-07 23:45:02 +0530

    --- Auto-Git Commit ---

--- src/Components/Admin/UpdateMobile.js
+++ src/Components/Admin/UpdateMobile.js
@@ -6,7 +6,7 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-
+import { TextField } from "@mui/material";
 const UpdateMobile = () => {
   const [data, setData] = useState({});
   const [updatedData, setUpdatedData] = useState({});
@@ -35,9 +35,8 @@ const UpdateMobile = () => {
             <Table sx={{ minWidth: 500 }} aria-label="simple table">
               <TableBody>
                 {data.specification?.map((row) => {
-                  
                   if (row.value) {
-                  return (
+                    return (
                       <TableRow
                         key={row.name}
                         sx={{
@@ -49,10 +48,10 @@ const UpdateMobile = () => {
                         </TableCell>
                         <TableCell align="left">{row.value}</TableCell>
                       </TableRow>
-                    )
-                  }else{
-                  return null;
-                }
+                    );
+                  } else {
+                    return null;
+                  }
                 })}
               </TableBody>
             </Table>
@@ -79,47 +78,161 @@ const UpdateMobile = () => {
             <div className="boxLayout d-flex-justify-align">abc5</div>
           )}
           {data.weight === -1 && (
-            <div className="boxLayout d-flex-justify-align">
-              <div className="questions container">
-                <h6 className="col-6">What is the weight of the phone?</h6>
+            <div className="boxLayout">
+              <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  What is the weight of the phone?
+                </h6>
+                <TextField
+                  id="standard-basic"
+                  label="Wts. in gram?"
+                  variant="standard"
+                  className="col-6"
+                />
               </div>
             </div>
           )}
           {!data.dualSim && (
             <div className="boxLayout">
-              <div className="questions row">
-                <h6 className="col-6">Does the phone has Dual SIM?</h6>
+              <div className="questions row p-4">
+                <h6 className="col-6 my-auto">Does the phone has Dual SIM?</h6>
                 <Select
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={true}
-                                label="Age"
-                                className='col-6'
-                            // onChange={handleChange}
-                            >
-                                <MenuItem value={true}>Yes</MenuItem>
-                                <MenuItem value={false}>No</MenuItem>
-                            </Select>
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={true}
+                  label="Age"
+                  className="col-6"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={true}>Yes</MenuItem>
+                  <MenuItem value={false}>No</MenuItem>
+                </Select>
               </div>
             </div>
           )}
           {data.ipxRating && (
-            <div className="boxLayout d-flex-justify-align">abc8</div>
+            <div className="boxLayout ">
+              <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  Does the phone any IPx Ratings?
+                </h6>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={"IP69"}
+                  label="Age"
+                  className="col-6"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={"IP69"}>IP69</MenuItem>
+                  <MenuItem value={"IP68"}>IP68</MenuItem>
+                  <MenuItem value={"IP67"}>IP67</MenuItem>
+                  <MenuItem value={"IP66"}>IP66</MenuItem>
+                  <MenuItem value={"IP65"}>IP65</MenuItem>
+                  <MenuItem value={"IP64"}>IP64</MenuItem>
+                  <MenuItem value={"IP63"}>IP63</MenuItem>
+                  <MenuItem value={"IP62"}>IP62</MenuItem>
+                  <MenuItem value={"IP61"}>IP61</MenuItem>
+                  <MenuItem value={"IP60"}>IP60</MenuItem>
+                  <MenuItem value={"IP59"}>IP59</MenuItem>
+                  <MenuItem value={"IP58"}>IP58</MenuItem>
+                  <MenuItem value={"IP57"}>IP57</MenuItem>
+                  <MenuItem value={"IP56"}>IP56</MenuItem>
+                  <MenuItem value={"IP55"}>IP55</MenuItem>
+                  <MenuItem value={"IP54"}>IP54</MenuItem>
+                  <MenuItem value={"IP53"}>IP53</MenuItem>
+                  <MenuItem value={null}>None</MenuItem>
+                </Select>
+              </div>
+            </div>
           )}
           {data.displayType === null && (
-            <div className="boxLayout d-flex-justify-align">abc9</div>
+            <div className="boxLayout">
+              <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  Please select type of Display?
+                </h6>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={"Amoled"}
+                  label="Age"
+                  className="col-6"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={"Amoled"}>Amoled</MenuItem>
+                  <MenuItem value={"IPS"}>IPS</MenuItem>
+                  <MenuItem value={"LCD"}>LCD</MenuItem>
+                </Select>
+              </div>
+            </div>
           )}
           {data.displayHz === -1 && (
-            <div className="boxLayout d-flex-justify-align">abc10</div>
+            <div className="boxLayout">
+                <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  Please select Refresh Rate of Display?
+                </h6>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={144}
+                  label="Age"
+                  className="col-6"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={60}>60Hz</MenuItem>
+                  <MenuItem value={90}>90Hz</MenuItem>
+                  <MenuItem value={120}>120Hz</MenuItem>
+                  <MenuItem value={144}>144Hz</MenuItem>
+                </Select>
+              </div>
+            </div>
           )}
-          {data.displayResolutionHeight === -1 && (
-            <div className="boxLayout d-flex-justify-align">abc11</div>
+          {data.displayResolutionHeight === -1 && data.displayResolutionWidth === -1 && (
+            <div className="boxLayout d-flex-justify-align">
+                <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  What is the Resolution of the phone?
+                </h6>
+                <TextField
+                  id="standard-basic"
+                  label="Width"
+                  variant="standard"
+                  className="col-2 pl-2"
+                />
+                <h6 className="col-2 d-grid justify-items-center align-items-end">X</h6>
+                <TextField
+                  id="standard-basic"
+                  label="Height"
+                  variant="standard"
+                  className="col-2 pl-2"
+                />
+              </div>
+            </div>
           )}
-          {data.displayResolutionWidth === -1 && (
-            <div className="boxLayout d-flex-justify-align">abc12</div>
-          )}
+          
           {data.os === null && (
-            <div className="boxLayout d-flex-justify-align">abc13</div>
+            <div className="boxLayout">
+                <div className="questions row p-4">
+                <h6 className="col-6 my-auto">
+                  Please select OS of the Phone?
+                </h6>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={'IOS'}
+                  label="Age"
+                  className="col-6"
+                  // onChange={handleChange}
+                >
+                  <MenuItem value={'Android'}>Android</MenuItem>
+                  <MenuItem value={'IOS'}>IOS</MenuItem>
+                  <MenuItem value={'Symbian'}>Symbian</MenuItem>
+                  <MenuItem value={'Java'}>Java</MenuItem>
+                </Select>
+              </div>
+            </div>
           )}
           {data.cpuNoOfCores === -1 && (
             <div className="boxLayout d-flex-justify-align">abc14</div>

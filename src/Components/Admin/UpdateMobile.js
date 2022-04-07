59535c3935c2cfdfddc7ff1b0cdf4d5c33b34df0
braincodeishan <braincodeishan@gmail.com>
2022-04-07 14:24:13 +0530

    --- Auto-Git Commit ---

--- src/Components/Admin/UpdateMobile.js
+++ src/Components/Admin/UpdateMobile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import '../../Assets/CSS/Admin.css'
 const UpdateMobile = () => {
     const [data, setData] = useState({});
     const [updatedData,setUpdatedData]=useState({})
@@ -24,38 +25,38 @@ const UpdateMobile = () => {
 
     return (
         <div className="container">
-            <div className="boxLayout">
+            <div className="boxLayout d-flex-justify-align">
                 <h2>{data.name}</h2>
             </div>
-            {!data.twoG && < div className="boxLayout">
+            {!data.twoG && < div className="boxLayout d-flex-justify-align">
                 abc1
             </div>}
-            {!data.threeG && <div className="boxLayout">
+            {!data.threeG && <div className="boxLayout d-flex-justify-align">
                 abc2
             </div>}
-            {!data.fourG && <div className="boxLayout">abc3</div>}
-            {!data.fiveG && <div className="boxLayout">abc4</div>}
-            {data.announced === null && <div className="boxLayout">abc5</div>}
-            {data.weight === -1 && <div className="boxLayout">abc6</div>}
-            {!data.dualSim && <div className="boxLayout">abc7</div>}
-            {data.ipxRating && <div className="boxLayout">abc8</div>}
-            {data.displayType === null && <div className="boxLayout">abc9</div>}
-            {data.displayHz === -1 && <div className="boxLayout">abc10</div>}
-            {data.displayResolutionHeight === -1 && <div className="boxLayout">abc11</div>}
-            {data.displayResolutionWidth === -1 && <div className="boxLayout">abc12</div>}
-            {data.os === null && <div className="boxLayout">abc13</div>}
-            {data.cpuNoOfCores === -1 && <div className="boxLayout">abc14</div>}
-            {data.cpuHz === -1 && <div className="boxLayout">abc15</div>}
-            {!data.cardSlot && <div className="boxLayout">abc16</div>}
-            {data.pCamera && <div className="boxLayout">abc17</div>}
-            {data.pVideoResolution && <div className="boxLayout">abc18</div>}
-            {data.sCamera && <div className="boxLayout">abc19</div>}
-            {data.sVideoResolution && <div className="boxLayout">abc20</div>}
-            {data.cardSlot && <div className="boxLayout">abc21</div>}
-            {data.cardSlot && <div className="boxLayout">abc22</div>}
-            {data.cardSlot && <div className="boxLayout">abc23</div>}
-            {data.cardSlot && <div className="boxLayout">abc24</div>}
-            {data.cardSlot && <div className="boxLayout">abc25</div>}
+            {!data.fourG && <div className="boxLayout d-flex-justify-align">abc3</div>}
+            {!data.fiveG && <div className="boxLayout d-flex-justify-align">abc4</div>}
+            {data.announced === null && <div className="boxLayout d-flex-justify-align">abc5</div>}
+            {data.weight === -1 && <div className="boxLayout d-flex-justify-align">abc6</div>}
+            {!data.dualSim && <div className="boxLayout d-flex-justify-align">abc7</div>}
+            {data.ipxRating && <div className="boxLayout d-flex-justify-align">abc8</div>}
+            {data.displayType === null && <div className="boxLayout d-flex-justify-align">abc9</div>}
+            {data.displayHz === -1 && <div className="boxLayout d-flex-justify-align">abc10</div>}
+            {data.displayResolutionHeight === -1 && <div className="boxLayout d-flex-justify-align">abc11</div>}
+            {data.displayResolutionWidth === -1 && <div className="boxLayout d-flex-justify-align">abc12</div>}
+            {data.os === null && <div className="boxLayout d-flex-justify-align">abc13</div>}
+            {data.cpuNoOfCores === -1 && <div className="boxLayout d-flex-justify-align">abc14</div>}
+            {data.cpuHz === -1 && <div className="boxLayout d-flex-justify-align">abc15</div>}
+            {!data.cardSlot && <div className="boxLayout d-flex-justify-align">abc16</div>}
+            {data.pCamera && <div className="boxLayout d-flex-justify-align">abc17</div>}
+            {data.pVideoResolution && <div className="boxLayout d-flex-justify-align">abc18</div>}
+            {data.sCamera && <div className="boxLayout d-flex-justify-align">abc19</div>}
+            {data.sVideoResolution && <div className="boxLayout d-flex-justify-align">abc20</div>}
+            {data.cardSlot && <div className="boxLayout d-flex-justify-align">abc21</div>}
+            {data.cardSlot && <div className="boxLayout d-flex-justify-align">abc22</div>}
+            {data.cardSlot && <div className="boxLayout d-flex-justify-align">abc23</div>}
+            {data.cardSlot && <div className="boxLayout d-flex-justify-align">abc24</div>}
+            {data.cardSlot && <div className="boxLayout d-flex-justify-align">abc25</div>}
         </div >
     )
 }

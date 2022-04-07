01da3ee5c16846e74f55bf3a42dc7e206c6fa2ad
braincodeishan <braincodeishan@gmail.com>
2022-04-07 15:25:00 +0530

    --- Auto-Git Commit ---

--- src/Components/Admin/UpdateMobile.js
+++ src/Components/Admin/UpdateMobile.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import '../../Assets/CSS/Admin.css'
-import { Select } from '@mui/material';
+import { Select, MenuItem } from '@mui/material';
+
 const UpdateMobile = () => {
     const [data, setData] = useState({});
     const [updatedData, setUpdatedData] = useState({})
@@ -32,7 +33,7 @@ const UpdateMobile = () => {
             {!data.twoG && < div className="boxLayout d-flex-justify-align">
                 <div className="questions">
                     <h6>Is 2G available in the phone?</h6>
-                    <Select/>
+                    <Select />
                 </div>
             </div>}
             {!data.threeG && <div className="boxLayout d-flex-justify-align">
@@ -42,12 +43,22 @@ const UpdateMobile = () => {
             {!data.fiveG && <div className="boxLayout d-flex-justify-align">abc4</div>}
             {data.announced === null && <div className="boxLayout d-flex-justify-align">abc5</div>}
             {data.weight === -1 && <div className="boxLayout d-flex-justify-align">
-            <div className="questions">
-                    <h6>What is the weight of the Phone?</h6>
-                    <Select/>
+                <div className="questions d-grid">
+                <h6>What is the weight of the phone?</h6>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={age}
+                        label="Age"
+                        // onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
                 </div>
-                
-                </div>}
+
+            </div>}
             {!data.dualSim && <div className="boxLayout d-flex-justify-align">abc7</div>}
             {data.ipxRating && <div className="boxLayout d-flex-justify-align">abc8</div>}
             {data.displayType === null && <div className="boxLayout d-flex-justify-align">abc9</div>}
